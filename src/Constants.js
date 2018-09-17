43ee341f8d9c006d6b7ae50945bf7dc9a7a878b3
Vaibhav <Vaibhav.singh@hashedin.com>
2018-09-18 01:13:28 +0530

    added constants

---
+++ src/Constants.js
@@ -0,0 +1,6 @@
+
+// Data Tooltip Text
+export const DATA_TOOLTIP = "<b>Sample Commands</b><br/>Place X Y F</br>Move</br>Report</br>Left </br>Right</br>";
+
+export const GRID_HEIGHT = 5;
+export const GRID_WIDTH = 5;
